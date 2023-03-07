38ed2d76a76a797f2128784719d52f06f16cb906
Kermo <kermo.ohno@voco.com>
2023-03-07 10:58:36 +0200

    Added data json file and display it on view dynamically

--- index.js
+++ index.js
@@ -9,8 +9,22 @@ app.set('views', path.join(__dirname, 'views'))
 // static files - CSS, JS, Images
 app.use(express.static(path.join(__dirname, 'public')))
 
+//use file system - read and write files
+const fs = require('fs')
+
 app.get('/', (req, res) => {
-    res.render('index')
+    fs.readFile('./tasks.json', 'utf-8', (err, jsonString) => {
+        if(err){
+            console.log('Error reading file: ', err)
+            return
+        }
+        try {
+            const tasks = JSON.parse(jsonString)
+            res.render('index', {tasksData : tasks})
+        } catch (err) {
+            console.log('Error parsing JSON file: ', err)
+        }
+    })
 })
 
 app.listen(3002)
